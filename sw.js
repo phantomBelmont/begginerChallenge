bf01c91a73ce930adb9f0bb03515b47b98203c50
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-07-13 13:38:15 +0900

    sw.js の作成

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './body.css',
+  './device.css',
+  './stars.css',
+  './swap.js', 
+  './flip.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
