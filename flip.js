397fd83fd62fd4eabc60e04149a67a124c96cdb3
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-07-15 21:15:26 +0900

    Update flip.js

--- flip.js
+++ flip.js
@@ -1,10 +1,61 @@
-let surfaceToFlip=true;
+    const qNa=[{q:"🌕",a:"🐺",category:"metamorphosis"},{q:"🧛🏻",a:"🦇",category:"metamorphosis"},{q:"🐣",a:"🐦‍🔥",category:"metamorphosis"},{q:"🗡️",a:"🛡️",category:"others"}];
+    const cardUlooking = document.getElementById('divFlipCard');
+    let indexUlooking = 0;
+    let surface = true;
 
-        function surfaceFlip(){
-            surfaceToFlip=!surfaceToFlip;
-            if(surfaceToFlip){
-            document.getElementById('idflip').textContent="🌙";
-            }else{
-            document.getElementById('idflip').textContent="🐺";
-            };
+    let categoryToShow = 'all';
+
+        function categoryLabelling(chosenCateg){
+            categoryToShow = chosenCateg;
+            indexUlooking = 0;
+            categorizeNrender();
         }
+
+        let categorizedArray = [];
+
+        function render(){
+            if(categorizedArray.length === 0){
+                cardUlooking.textContent = "カード準備中😅";
+                return;
+            }
+            cardUlooking.textContent = surface ? categorizedArray[indexUlooking].q : categorizedArray[indexUlooking].a;
+        }
+
+        function categorize(){
+            if(categoryToShow === 'all'){
+                return qNa;
+            }else{
+                return qNa.filter(EachInqNa => EachInqNa.category === categoryToShow);
+            }
+        }
+
+        function categorizeNrender(){
+            categorizedArray = categorize();
+            render();
+        }
+
+        function flipCard(){
+            surface=!surface;
+            render();
+        }
+        function shuffleFY(){
+            for(let indexLast = qNa.length-1; indexLast > 0; indexLast--){
+                let indexLast0toLast = Math.floor(Math.random()*(indexLast+1));
+                [qNa[indexLast], qNa[indexLast0toLast]] = [qNa[indexLast0toLast], qNa[indexLast]];
+            }
+
+            categorizedArray = categorize();
+        }
+
+        function toNext(){
+            indexUlooking++;
+            if(indexUlooking >= categorizedArray.length){
+                shuffleFY();
+                indexUlooking = 0;
+            }
+            
+            surface = true;
+            render();
+        }
+
+        categorizeNrender();
