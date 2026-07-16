a08e8907a858d65a47b33017f1cca825f5318724
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-07-16 12:12:41 +0900

    Update flip.js

--- flip.js
+++ flip.js
@@ -1,50 +1,37 @@
-    const qNa=[{q:"🌕",a:"🐺",category:"metamorphosis"},{q:"🧛🏻",a:"🦇",category:"metamorphosis"},{q:"🐣",a:"🐦‍🔥",category:"metamorphosis"},{q:"🗡️",a:"🛡️",category:"others"}];
+    const TotalqNa=[{q:"🌕",a:"🐺",category:"metamorphosis"},{q:"🧛🏻",a:"🦇",category:"metamorphosis"},{q:"🐣",a:"🐦‍🔥",category:"metamorphosis"},{q:"🗡️",a:"🛡️",category:"others"}];
     const cardUlooking = document.getElementById('divFlipCard');
     let indexUlooking = 0;
     let surface = true;
 
-    let categoryToShow = 'all';
+    let chosenCategory = 'all';
+    let categorizedArray = [];
 
-        function categoryLabelling(chosenCateg){
-            categoryToShow = chosenCateg;
-            indexUlooking = 0;
-            categorizeNrender();
+        function openTheCategory(chosenCa){
+            chosenCategory = chosenCa;         
+            categorizedArray = pickupNmakeArray();
+            renderAcard();
+            indexUlooking = 0;            
         }
 
-        let categorizedArray = [];
+        function pickupNmakeArray(){
+            if(chosenCategory === 'all'){
+                return TotalqNa;
+            }else{
+                return TotalqNa.filter(EachInqNa => EachInqNa.category === chosenCategory);
+            }
+        }
 
-        function render(){
+        function renderAcard(){
             if(categorizedArray.length === 0){
-                cardUlooking.textContent = "カード準備中😅";
+                cardUlooking.textContent = "カード準備中🙇‍♀️";
                 return;
             }
             cardUlooking.textContent = surface ? categorizedArray[indexUlooking].q : categorizedArray[indexUlooking].a;
         }
 
-        function categorize(){
-            if(categoryToShow === 'all'){
-                return qNa;
-            }else{
-                return qNa.filter(EachInqNa => EachInqNa.category === categoryToShow);
-            }
-        }
-
-        function categorizeNrender(){
-            categorizedArray = categorize();
-            render();
-        }
-
         function flipCard(){
             surface=!surface;
-            render();
-        }
-        function shuffleFY(){
-            for(let indexLast = qNa.length-1; indexLast > 0; indexLast--){
-                let indexLast0toLast = Math.floor(Math.random()*(indexLast+1));
-                [qNa[indexLast], qNa[indexLast0toLast]] = [qNa[indexLast0toLast], qNa[indexLast]];
-            }
-
-            categorizedArray = categorize();
+            renderAcard();
         }
 
         function toNext(){
@@ -53,9 +40,16 @@
                 shuffleFY();
                 indexUlooking = 0;
             }
-            
             surface = true;
-            render();
+            renderAcard();
         }
-
-        categorizeNrender();
+        function shuffleFY(){
+                    for(let indexLast = TotalqNa.length-1; indexLast > 0; indexLast--){
+                        let index0toLast = Math.floor(Math.random()*(indexLast+1));
+                        [TotalqNa[indexLast], TotalqNa[index0toLast]] = [TotalqNa[index0toLast], TotalqNa[indexLast]];
+                    }
+                    categorizedArray = pickupNmakeArray();
+                }   
+                
+    categorizedArray = pickupNmakeArray();
+    renderAcard();
