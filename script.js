acc948695fc9df955fb9050f1e7876e84e312d03
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-07-28 15:38:09 +0900

    Add service worker registration in script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//サービスワーカー
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+          .then(registration => {
+            console.log('SW registered: ', registration);
+          })
+          .catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+          });
+      });
+    }
+ 
 
                 //XSS対策
                     document.addEventListener('DOMContentLoaded', () => {
