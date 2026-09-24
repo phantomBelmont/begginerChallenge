e12ce66b9f10d4c386a1ed83946968d2f856a863
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-09-24 21:16:13 +0900

    sw.js の作成

---
+++ sw.js
@@ -0,0 +1,55 @@
+const tryCACHE = 'v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './script.js',
+  './style.css',
+  './manifest.json',
+  './icon512.png',
+  './icon192.png'
+];
+
+self.addEventListener(
+  'install', ins=>{
+    ins.waitUntil(
+      caches.open(tryCACHE).then(tryCA=>tryCA.addAll(ASSETS))
+      .then(()=>createCACHE.skipWaiting()
+      )//thenここまで
+    );//waitUntilここまで
+});//イベリスここまで
+
+//次に古いキャッシュの削除
+self.addEventListener('activate',acti=>{acti.waitUntil(
+  caches.keys()
+  .then(KEYS=>Promise.all(KEYS.filter(k=> k !== tryCACHE)
+  .map(k=>caches.delete(k)
+  )//mapここまで
+  )//Promise.allここまで
+  )//caches.keys().thenここまで
+  .then(()=>clients.claim()
+  )//クライアントクレームを始めるthenここまで
+);//waitUntilここまで
+});//イベリスここまで
+
+//中身を取ってくる
+self.addEventListener('fetch',fe=>{
+  if(fe.request.method !== 'GET')return;
+
+  fe.respondWith(
+
+    caches.match(fe.request).then(cachedRes=>{
+    const fetchPromise=fetch(fe.request).then(networkRes=>{
+      if(networkRes && networkRes.status===200){
+        const netCopy=networkRes.clone();
+        caches.open(tryCACHE).then(tryCA=>tryCA.put(fe.request,netCopy));//open().then
+        }//if
+      return networkRes;
+    }//fetch().then arrow
+    )//fetch().then
+    .catch(()=>{});
+
+    return cachedRes || fetchPromise
+  }//match().then arrow
+  )//match().then
+  )//respondWith
+});//イベリスここまで
